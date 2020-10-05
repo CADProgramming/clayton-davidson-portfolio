6cf1563e3526e9b35d14b0588b7b002b1cb4bc9a
Clayton Davidson <casd693@gmail.com>
2020-10-05 17:57:38 +1300

    Trying to fix posts foreign key link

--- database/migrations/2020_07_27_235523_create_weeks_table.php
+++ database/migrations/2020_07_27_235523_create_weeks_table.php
@@ -14,7 +14,7 @@ class CreateWeeksTable extends Migration
     public function up()
     {
         Schema::create('weeks', function (Blueprint $table) {
-            $table->bigInteger('id');
+            $table->unsignedBigInteger('id');
             $table->timestamp('week_start')->nullable();
             $table->timestamp('week_end')->nullable();
             $table->timestamps();
